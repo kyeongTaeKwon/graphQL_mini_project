34bc40b6cdce1d4d266d06c1803586f7e5d861c3
kwon <sjbby100@naver.com>
2020-08-07 23:00:06 +0900

    add book schema(rootQuery for GraphQlSchema)

---
+++ server/schema/schema.js
@@ -0,0 +1,29 @@
+const graphql = require("graphql");
+
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+const BookType = new GraphQLObjectType({
+  name: "Book",
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString },
+  }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+        // 데이터베이스,다른 곳 에서 데이터를 가져온다
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
