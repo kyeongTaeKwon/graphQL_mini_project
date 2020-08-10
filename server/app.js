58dbd1d50e83bd0419aab21255f4b07fc491b897
kwon <sjbby100@naver.com>
2020-08-10 17:33:09 +0900

    Add cors for same origin(client)

--- server/app.js
+++ server/app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const graphqlHTTP = require("express-graphql").graphqlHTTP;
 const schema = require("./schema/schema");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 mongoose
   .connect(
     "mongodb+srv://kwon:!rudxo7716@cluster0.hslgl.mongodb.net/graphql_kwon?retryWrites=true&w=majority",
